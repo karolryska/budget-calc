65e3bae20d1420d04b18ba848485f7e28e5d66fe
Karol Ryska <karolryska@gmail.com>
2021-11-21 19:16:00 +0100

    feat: add size properties based on props

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,16 +1,24 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.section`
-    height: ${(props) => props.height};
+    flex-grow: ${(props) => props.flexGrow && '1'};
+    height: ${(props) => props.height && props.height};
     width: 100%;
     padding: 16px;
     margin-bottom: 20px;
     border-radius: 20px;
     background-color: #fafafa;
+    &:last-child {
+        margin-bottom: 0;
+    }
 `;
 
-const Section = ({ children, height }) => {
-    return <Wrapper height={height}>{children}</Wrapper>;
+const Section = ({ children, height, flexGrow }) => {
+    return (
+        <Wrapper height={height} flexGrow={flexGrow}>
+            {children}
+        </Wrapper>
+    );
 };
 
 export default Section;
