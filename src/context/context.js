b4de99883b2359f589534df0dcfd3c3bc445f48a
Karol Ryska <karolryska@gmail.com>
2021-11-28 18:42:02 +0100

    feat: add categories sum to app provider and change local storage object

--- src/context/context.js
+++ src/context/context.js
@@ -1,9 +1,11 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import { saveData, getData } from 'helpers/manageData';
 import { sumRecords } from 'helpers/sumRecords';
+import { sumCategories } from 'helpers/categories';
 
 const context = {
     sum: 0,
+    categoriesSum: {},
     store: [],
     updateStore: null,
 };
@@ -11,17 +13,28 @@ const context = {
 export const Storage = createContext(context);
 
 const AppProvider = ({ children }) => {
-    const [items, setItems] = useState(getData());
-    const [sum, setSum] = useState(sumRecords(items));
+    const [items, setItems] = useState(getData().items);
+    const [sum, setSum] = useState(getData().sum);
+    const [categoriesSum, setCategoriesSum] = useState(getData().categoriesSum);
+
     const updateItems = (data) => {
-        setSum(sumRecords(data));
         setItems(data);
-        saveData(data);
+        setSum(sumRecords(data));
+        setCategoriesSum(sumCategories(data));
     };
+
+    useEffect(() =>
+        saveData({ items, sum, categoriesSum }, [items, sum, categoriesSum])
+    );
 
     return (
         <Storage.Provider
-            value={{ sum: sum, store: items, updateStore: updateItems }}
+            value={{
+                sum: sum,
+                store: items,
+                categoriesSum: categoriesSum,
+                updateStore: updateItems,
+            }}
         >
             {children}
         </Storage.Provider>
