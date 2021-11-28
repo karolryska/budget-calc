41f2ff5bd72a969ac7b3cf04be9fdfe843e3a0b8
Karol Ryska <karolryska@gmail.com>
2021-11-28 18:39:40 +0100

    feat: change return - biggest scope of local storage

--- src/helpers/manageData.js
+++ src/helpers/manageData.js
@@ -6,6 +6,6 @@ export const getData = () => {
     if (localStorage.getItem('storage')) {
         return JSON.parse(localStorage.getItem('storage'));
     } else {
-        return [];
+        return { items: [], sum: 0, categoriesSum: {} };
     }
 };
