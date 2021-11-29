997a45773c58c5e46efd1525256f7d18e6ec553d
Karol Ryska <karolryska@gmail.com>
2021-11-29 22:14:32 +0100

    refactor: remove console.log

--- src/helpers/sumRecords.js
+++ src/helpers/sumRecords.js
@@ -1,7 +1,6 @@
 export const sumRecords = (data) => {
     let sum = 0;
     data.forEach((record) => {
-        console.log(typeof record.price);
         record.type === 'income'
             ? (sum += record.price)
             : (sum -= record.price);
