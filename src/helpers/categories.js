101e443607d27a804fe2068ab7a00e39db4ce5c7
Karol Ryska <karolryska@gmail.com>
2021-12-23 12:27:09 +0100

    feat: sum only records type passed as argument

--- src/helpers/categories.js
+++ src/helpers/categories.js
@@ -9,16 +9,20 @@ export const expenses = [
     'other',
 ];
 
-export const sumCategories = (data) => {
+export const sumCategories = (data, currentType) => {
+    const currentTypeRecords = data.filter(
+        (record) => record.type === currentType
+    );
+
     const categories = [];
 
-    data.forEach((record) => {
+    currentTypeRecords.forEach((record) => {
         const index = categories.findIndex(
             (category) => category.name === record.category
         );
-        if (record.type === 'expense' && categories[index]) {
+        if (categories[index]) {
             categories[index].sum += record.price;
-        } else if (record.type === 'expense' && !categories[index]) {
+        } else {
             categories.push({ name: record.category, sum: record.price });
         }
     });
