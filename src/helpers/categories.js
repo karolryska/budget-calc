e21d20bd5fa2d14e23f3afc9c2a3d4b6cfe373dc
Karol Ryska <karolryska@gmail.com>
2022-01-03 22:59:52 +0100

    feat: add color to categories

--- src/helpers/categories.js
+++ src/helpers/categories.js
@@ -1,13 +1,54 @@
-export const incomes = ['salary', 'other'];
-export const expenses = [
-    'bills',
-    'car',
-    'clothes',
-    'coffee',
-    'groceries',
-    'restaurant',
-    'other',
-];
+export const incomes = {
+    bonus: {
+        color: '#ff5722',
+    },
+    salary: {
+        color: '#ffc107',
+    },
+    other: {
+        color: '#cddc39',
+    },
+};
+
+export const expenses = {
+    bills: {
+        color: '#f44336',
+    },
+    car: {
+        color: '#e91e63',
+    },
+    clothes: {
+        color: '#9c27b0',
+    },
+    coffee: {
+        color: '#3f51b5',
+    },
+    groceries: {
+        color: '#03a9f4',
+    },
+    restaurant: {
+        color: '#009688',
+    },
+    other: {
+        color: '#8bc34a',
+    },
+};
+
+export const incomesList = () => {
+    const list = [];
+    for (const key in incomes) {
+        list.push(key);
+    }
+    return list;
+};
+
+export const expensesList = () => {
+    const list = [];
+    for (const key in expenses) {
+        list.push(key);
+    }
+    return list;
+};
 
 export const sumCategories = (data, currentType) => {
     const currentTypeRecords = data.filter(
