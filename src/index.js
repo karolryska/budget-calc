f59f6768e4e260e3074516ce6b199f0626bee7f4
Karol Ryska <karolryska@gmail.com>
2022-01-06 22:20:05 +0100

    feat: add theme provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppProvider from 'context/context';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'styles/theme';
 import GlobalStyle from 'styles/globalStyle';
 import App from 'views/App';
 
 ReactDOM.render(
     <React.StrictMode>
         <AppProvider>
-            <GlobalStyle />
-            <App />
+            <ThemeProvider theme={theme}>
+                <GlobalStyle />
+                <App />
+            </ThemeProvider>
         </AppProvider>
     </React.StrictMode>,
     document.getElementById('root')
