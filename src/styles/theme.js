1365027b554058e9eeb28c1bd30a65a938244c21
Karol Ryska <karolryska@gmail.com>
2022-01-06 22:19:53 +0100

    feat: add theme

---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+    colors: {
+        white: '#ffffff',
+        lightGrey: '#fafafa',
+        grey: '#eeeeee',
+        blue: '#1976d2',
+        black: '#111111',
+    },
+};
