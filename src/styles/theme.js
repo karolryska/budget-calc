e1bb28ed68cbd6a9627c8a4a6dfadbd245c08498
Karol Ryska <karolryska@gmail.com>
2022-01-08 19:52:04 +0100

    feat: add dark grey color

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,6 +3,7 @@ export const theme = {
         white: '#ffffff',
         lightGrey: '#fafafa',
         grey: '#eeeeee',
+        darkGrey: '#bdbdbd',
         blue: '#1976d2',
         blueHover: '#1565c0',
         black: '#111111',
